ca7c57683df4098951c55b7742b3580b4c3e60b9
melpatjen16 <mp.jenkins16@gmail.com>
2022-10-12 13:46:51 -0400

    Update

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -3,6 +3,12 @@ mecha_cars<- read.csv(file="MechaCar_mpg.csv")
 library (dplyr)
 library (ggplot2)
 
+#Deliverable 1
+
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_cars) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_cars)) #generate summary statistics
 
+#Deliverable 2
+
+summarize_demo <- demo_table2 %>% group_by(condition) %>% summarize(Mean_Mileage=mean(odometer), .groups = 'keep') #create summary table
+summarize_demo <- demo_table2 %>% group_by(condition) %>% summarize(Mean_Mileage=mean(odometer),Maximum_Price=max(price),Num_Vehicles=n(), .groups = 'keep') #create summary table with multiple columns
